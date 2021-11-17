82e2c03970a9e004e5209c730f4a57dd53dff7da
Matti Luukkainen <mluukkai@iki.fi>
2021-11-17 17:29:04 +0200

    viikko4 java

---
+++ koodi/viikko4/TddOstoskori/src/test/java/ohtu/OstoskoriTest.java
@@ -0,0 +1,24 @@
+package ohtu;
+
+import java.util.Arrays;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class OstoskoriTest {
+
+    Ostoskori kori;
+
+    @Before
+    public void setUp() {
+        kori = new Ostoskori();
+    }
+
+    @Test
+    public void ostoskorinHintaJaTavaroidenMaaraAlussa() { 
+        assertEquals(0, kori.hinta());
+ 
+        // ...
+    }
+
+}
